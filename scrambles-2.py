15b7853713487cde7ea11d1fe3bcde61c7fdda8b
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-12-13 09:21:21 +0530

    aaaaaaaaaaa

--- scrambles-2.py
+++
@@ -1,8 +0,0 @@
-def scramble(s1, s2):
-    for i in s2:
-        if i in s1:
-            s1 = s1.replace(i,'',1)
-        else:
-            return False
-    return True
-    
--- scrambles.py
+++ scrambles.py
@@ -1,7 +1,16 @@
 def scramble(s1, s2):
     for i in s2:
-        if i not in s1:
+        if i in s1:
+            s1 = s1.replace(i,'',1)
+        else:
             return False
-        s1 = s1.replace(i,'',1)
     return True
+    
+#  This approach only passes 510 test cases while the one above passes 512 but both timeout for some reason
+# def scramble(s1, s2):
+#     for i in s2:
+#         if i not in s1:
+#             return False
+#         s1 = s1.replace(i,'',1)
+#     return True
             
